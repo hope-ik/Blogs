978e69b7ea4dd5148d139c2b0217f5f3569e1c05
hope-ik <68222348+hope-ik@users.noreply.github.com>
2020-12-15 17:31:29 +0800

    Add files via upload

--- js/dist/home.dev.js
+++ js/dist/home.dev.js
@@ -15,28 +15,34 @@
 //   return false;
 // });
 // // 技能列表
-// var progress = 0;
-// $(".skill-list li").on("mouseenter", function () {
-//   var val = $(this).attr("data-percent");
-//   $(this)
-//     .children(".skill-item")
-//     .children(".skill-img")
-//     .css("bottom", -(100 - val) + "%");
-//   function step() {
-//     progress += 2;
+var progress = 0;
+$(".skill-list li").on("mouseenter", function () {
+  var val = $(this).attr("data-percent"); //当前属性为data-percent的值赋值给val
+
+  $(this).children(".skill-item").children(".skill-img").css('bottom', -(100 - val) + '%'); //根据用户给定的值改变css高度
+
+  function step() {
+    progress += 2; //progress值加2
+
+    $(".skill-inscir .skill-per").html(progress + "%"); //往class为skill-per的标签里添加progress值
+
+    if (progress < val) {
+      requestAnimationFrame(step); //当progress小于val时进入
+    }
+  }
+
+  setTimeout(function () {
+    requestAnimationFrame(step);
+  }, 200); //200毫秒执行
+});
+$("#skill-list li").on("mouseleave", function () {
+  $('.skill-img').css("bottom", "-100px");
+  progress = 0;
+  $(".skill-inscir .skill-per").html(progress + "%");
+}); // $(".skill-list li").on("mouseleave", function () {
+//     $(".skill-img").css("bottom", "-100px");
+//     progress = 0;
 //     $(".skill-inscir .skill-per").html(progress + "%");
-//     if (progress < val) {
-//       requestAnimationFrame(step);
-//     }
-//   }
-//   setTimeout(function () {
-//     requestAnimationFrame(step);
-//   }, 200);
-// });
-// $(".skill-list li").on("mouseleave", function () {
-//   $(".skill-img").css("bottom", "-100px");
-//   progress = 0;
-//   $(".skill-inscir .skill-per").html(progress + "%");
 // });
 // //工作案例切换
 // $(".works-list-tab li").on("click", function () {
@@ -44,12 +50,54 @@
 //   $(".works .works-list").fadeOut("fast");
 //   $(".works .works-list").eq($(this).index()).fadeIn("fast");
 // });
-// // loading
+
+$(function () {
+  new WOW().init(); //初始化wow.js
+}); // loading
+
 window.onload = function () {
   $(".preloader").delay(1000).fadeOut("slow"); //一秒后缓慢隐没
-}; // // niceScroll
-// $("html").niceScroll();
-// // Stick menu
+}; //scrollReveal
+
+
+$(function () {
+  window.scrollReveal = new scrollReveal({
+    reset: true,
+    move: "100px"
+  });
+}); //技能列表
+// // niceScroll 滚动条
+
+$(function () {
+  $('html').niceScroll({
+    cursorcolor: "#f97352",
+    //滚动条的颜色
+    cursoropacitymax: 0.8,
+    //滚动条的透明度，从0-1   
+    touchbehavior: false,
+    //使光标拖动滚动像在台式电脑触摸设备 true滚动条拖动不可用  
+    cursorwidth: "6px",
+    //滚动条的宽度  单位默认px 
+    cursorborder: "0",
+    // 游标边框css定义    
+    cursorborderradius: "3px",
+    //滚动条两头的圆角   
+    autohidemode: false,
+    //是否隐藏滚动条  true的时候默认不显示滚动条，当鼠标经过的时候显示滚动条   
+    zindex: "auto",
+    //给滚动条设置z-index值    
+    spacebarenabled: true,
+    // 当按下空格时使页面向下滚动
+    railvalign: 'defaul',
+    railpadding: {
+      top: 0,
+      right: -6,
+      left: 0,
+      bottom: 0
+    } //滚动条的位置
+
+  });
+}); // // Stick menu
 // $("nav").sticky({ topSpacing: 0 });
 // // var navH = $("nav").offset().top;
 // //  //滚动条事件
@@ -62,15 +110,12 @@ window.onload = function () {
 // //   }
 // //  })
 // // 首页幻灯片
-
-
-var swiper = new Swiper(".swiper-container", {
-  autoplay: 3000,
-  //可选选项，自动滑动
-  pagination: ".swiper-pagination",
-  //添加滑动按钮
-  paginationClickable: true
-}); // // 移动端菜单
+// var swiper = new Swiper(".swiper-container", {
+//     autoplay: 3000, //可选选项，自动滑动
+//     pagination: ".swiper-pagination", //添加滑动按钮
+//     paginationClickable: true,
+// });
+// // 移动端菜单
 // $(document).on("click", ".m-nav-menu", function () {
 //   $(".m-nav-menu").toggleClass("on");
 //   $(".nav-menu").slideToggle();
@@ -80,6 +125,3 @@ var swiper = new Swiper(".swiper-container", {
 //     $(this).hide();
 //   });
 // }
-// (function () {
-//   window.scrollReveal = new scrollReveal({ reset: true, move: "100px" });
-// })();
