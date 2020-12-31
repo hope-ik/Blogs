20f9b42c0485f36d739c2c794db70ad7cd4c9eb8
hope-ik <68222348+hope-ik@users.noreply.github.com>
2020-12-31 13:27:42 +0800

    Add files via upload

--- js/dist/home.dev.js
+++ js/dist/home.dev.js
@@ -8,7 +8,7 @@
 // loading
 window.onload = function () {
   $(".preloader").delay(1000).fadeOut("slow"); //一秒后缓慢隐没
-}; // // Menu Scroll to content and Active menu
+}; // Menu Scroll to content and Active menu
 
 
 $("a[href*=#]").bind("click", function (e) {
@@ -31,7 +31,7 @@ window.onscroll = function (e) {
   } else {
     $("#menu").css("display", "none");
   }
-}; // // 技能列表
+}; // 技能列表
 
 
 var progress = 0;
@@ -60,17 +60,15 @@ $("#skill-list li").on("mouseleave", function () {
   progress = 0;
   $(".skill-inscir .skill-per").html(progress + "%");
 });
-$(function () {
-  new WOW().init(); //初始化wow.js
-}); //scrollReveal
+new WOW().init(); //初始化wow.js
+//scrollReveal
 
-$(function () {
-  var config = {
-    reset: true,
-    move: "100px"
-  };
-  window.scrollReveal = new scrollReveal(config); // console.log(window.scrollReveal)
-}); //工作案例切换
+var config = {
+  reset: true,
+  move: "100px"
+};
+window.scrollReveal = new scrollReveal(config); // console.log(window.scrollReveal)
+//工作案例切换
 
 $('.works-list-tab li').click(function () {
   //siblings() 方法返回被选元素的所有同级元素。
@@ -82,7 +80,7 @@ $('.works-list-tab li').click(function () {
 }); // // niceScroll 滚动条
 
 $('html').niceScroll({
-  cursorcolor: "#f97352",
+  cursorcolor: "#099",
   //滚动条的颜色
   cursoropacitymax: 0.8,
   //滚动条的透明度，从0-1   
@@ -112,6 +110,13 @@ $('html').niceScroll({
 //     this.css("color", "red")
 //     return this;
 // }
+// function foo(x) {
+//     console.log(arguments)
+//     for (let i = 0; i < arguments.length; i++) {
+//         console.log("arg" + i + "=" + arguments[i])
+//     }
+// }
+// foo(30, 20, 30)
 // // Stick menu
 // $("nav").sticky({ topSpacing: 0 });
 // // var navH = $("nav").offset().top;
--- js/home.js
+++ js/home.js
@@ -9,7 +9,7 @@ window.onload = function () {
     $(".preloader").delay(1000).fadeOut("slow"); //一秒后缓慢隐没
 }
 
-// // Menu Scroll to content and Active menu
+// Menu Scroll to content and Active menu
 
 $("a[href*=#]").bind("click", function (e) {
     // console.log(e.currentTarget.href)
@@ -17,7 +17,7 @@ $("a[href*=#]").bind("click", function (e) {
     var target = $(this).attr("href");//当前点击的href属性
     $("html,body")
         .stop()
-        .animate({ scrollTop: $(target).offset().top - 100 }, 1000, function () {});
+        .animate({ scrollTop: $(target).offset().top - 100 }, 1000, function () { });
     return false;
 });
 
@@ -34,7 +34,7 @@ window.onscroll = function (e) {
 }
 
 
-// // 技能列表
+// 技能列表
 var progress = 0;
 $(".skill-list li").on("mouseenter", function () {//鼠标移入事件
     let val = $(this).attr("data-percent");//当前属性为data-percent的值赋值给val
@@ -63,19 +63,17 @@ $("#skill-list li").on("mouseleave", function () {
 
 
 
-$(function () {
-    new WOW().init();//初始化wow.js
-})
+
+new WOW().init();//初始化wow.js
 
 //scrollReveal
-$(function () {
-    const config = {
-        reset: true,
-        move: "100px"
-    };
-    window.scrollReveal = new scrollReveal(config);
-    // console.log(window.scrollReveal)
-})
+const config = {
+    reset: true,
+    move: "100px"
+};
+window.scrollReveal = new scrollReveal(config);
+// console.log(window.scrollReveal)
+
 
 //工作案例切换
 $('.works-list-tab li').click(function () {
@@ -91,7 +89,7 @@ $('.works-list-tab li').click(function () {
 
 // // niceScroll 滚动条
 $('html').niceScroll({
-    cursorcolor: "#f97352", //滚动条的颜色
+    cursorcolor: "#099", //滚动条的颜色
     cursoropacitymax: 0.8, //滚动条的透明度，从0-1   
     touchbehavior: false, //使光标拖动滚动像在台式电脑触摸设备 true滚动条拖动不可用  
     cursorwidth: "6px", //滚动条的宽度  单位默认px 
@@ -113,6 +111,15 @@ $('html').niceScroll({
 //     this.css("color", "red")
 //     return this;
 // }
+
+// function foo(x) {
+//     console.log(arguments)
+//     for (let i = 0; i < arguments.length; i++) {
+//         console.log("arg" + i + "=" + arguments[i])
+//     }
+// }
+// foo(30, 20, 30)
+
 
 
 // // Stick menu
